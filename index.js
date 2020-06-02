4fcb1857200f162cc746e304883c55c3b4d138e2
Lucia Brammer <brammerl96@gmail.com>
2020-06-02 15:41:02 -0700

    fixing static issues on the note.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const minimist = require("minimist")
+const Input = require('./lib/Input.js');
+const Note = require('./lib/Note.js');
 
+const input = new Input();
+const note = new Note();
 
+const action = input.parse(process.argv);
 
-console.log(process.argv);
+input.valid(action)? note.execute(action): console.log('')
+
+const isValid = input.valid(action);
+
+if(isValid) note.execute(action);
+else console.logZ('invalid command')
