e94346b6b902f351dc7c298ef185e364424466cc
Lucia Brammer <brammerl96@gmail.com>
2020-06-03 16:22:57 -0700

    tests for list and add are working

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const Input = require('./lib/Input.js');
-const Note = require('./lib/Note.js');
+const input = require('./lib/Input.js');
+const Note = require('./lib/models/Note.js');
 
-const input = new Input();
-const note = new Note();
+
 
 const action = input.parse(process.argv);
 
-input.valid(action)? note.execute(action): console.log('')
+input.valid(action)? Note.execute(action): console.log('')
 
 const isValid = input.valid(action);
 
 if(isValid) note.execute(action);
-else console.logZ('invalid command')
+else console.log('invalid command')
